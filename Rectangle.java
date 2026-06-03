48a4d5439ff90b819458280a8d525ab9d1bb3a11
kavinGS10 <kavin.gs2025ece@sece.ac.in>
2026-06-03 12:36:44 +0530

    Update Rectangle.java

--- Rectangle.java
+++ Rectangle.java
@@ -1,17 +1,13 @@
 class Rectangle {
-    // Instance variables
+  
     int length;
     int breadth;
-
-    // Constructor
     Rectangle(int length, int breadth) {
         this.length = length;
         this.breadth = breadth;
     }
-
-    // Method using local variable
     void findArea() {
-        int area = length * breadth; // Local variable
+        int area = length * breadth; 
 
         System.out.println("Length = " + length);
         System.out.println("Breadth = " + breadth);
